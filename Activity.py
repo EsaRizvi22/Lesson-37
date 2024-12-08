4685a7359d225422fe13e49e2381a35dcc527952
esarizvi22 <honeyrizvi701@gmail.com>
2024-12-08 16:18:01 +0530

    Activity 1.py

---
+++ Activity.py
@@ -0,0 +1,7 @@
+#Comments can be written here
+print("Welcome to my fist python code.")
+
+print("I am Esa")
+print("I am 17 years old")
+print("I am learning Python language")
+print("I am passionate about coding")
